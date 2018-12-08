efb6d07131e3fea3be552d96782e80c0a754bcf0
Donglin <weidongl74@gmail.com>
2018-12-08 01:03:51 -0800

    Finish the second half

--- src/main/wc.go
+++ src/main/wc.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"mapreduce"
 	"os"
+	"strings"
+	"unicode"
+	"strconv"
 )
 
 //
@@ -14,9 +17,18 @@ import (
 // of key/value pairs.
 //
 func mapF(filename string, contents string) []mapreduce.KeyValue {
-	// Your code here (Part II).
-	fmt.Println(contents)
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c)
+	}
+
+	words := strings.FieldsFunc(contents, f)
+	
 	var kvs []mapreduce.KeyValue
+
+	for _, word := range words {
+		kvs = append(kvs, mapreduce.KeyValue{word, "1"})					
+	}
+
 	return kvs
 }
 
@@ -26,9 +38,8 @@ func mapF(filename string, contents string) []mapreduce.KeyValue {
 // any map task.
 //
 func reduceF(key string, values []string) string {
-	// Your code here (Part II).
-	ret := "dummy"
-	return ret
+	countStr := strconv.Itoa(len(values))
+	return countStr
 }
 
 // Can be run in 3 ways:
